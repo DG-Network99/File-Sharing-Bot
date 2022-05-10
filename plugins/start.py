427a1d67965c091b5f5998cc45354cdc84820bbb
DG-Network99 <105211286+DG-Network99@users.noreply.github.com>
2022-05-10 16:05:02 +0530

    add about and close

--- plugins/start.py
+++ plugins/start.py
@@ -90,6 +90,9 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
+                    
+                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
+                    InlineKeyboardButton("🔒 Close", callback_data = "close")
                    
                 ]
             ]
